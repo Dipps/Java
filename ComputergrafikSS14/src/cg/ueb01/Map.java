7c0aff9717f49f02ef4074bafdec2447516dd987
Jan Dieckmann <dipps86@gmail.com>
2014-04-19 21:15:00 +0200

    map.java changed

--- ComputergrafikSS14/src/cg/ueb01/Map.java
+++ ComputergrafikSS14/src/cg/ueb01/Map.java
@@ -5,17 +5,8 @@ import javax.swing.ImageIcon;
 public class Map
 {
 
-    ImageIcon image;
-
-    final private double[][] matrix;
-
-    final private double[][] iMatrix;
-
     public Map(final ImageIcon i)
     {
-        image = i;
-        matrix = new double[2][2];
-        iMatrix = new double[2][2];
 
     }
 
