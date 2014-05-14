d053bd479d257a0421047f4b63c218e9af46653b
Jan Dieckmann <dipps86@gmail.com>
2014-05-14 17:31:49 +0200

    Repaint bug fixed

--- ComputergrafikSS14/src/cg/seekarte/MapModel.java
+++ ComputergrafikSS14/src/cg/seekarte/MapModel.java
@@ -8,8 +8,7 @@ import cg.matrix.Matrix;
 import cg.punkteditor.Punkt;
 import cg.punkteditor.Punktliste;
 
-public class MapModel
-{
+public class MapModel {
     private final ArrayList<IMapModelListener> listeners;
 
     private Punktliste points;
@@ -24,163 +23,135 @@ public class MapModel
 
     private int mouseY;
 
-    public MapModel()
-    {
+    public MapModel() {
         listeners = new ArrayList<>();
         points = new Punktliste();
 
         // Kalibrierungs Punkte(Lotsekarte) als Vektor (L1, B1, x1, y1)
-        final double[] calLotsePoint1 =
-        { 56, 11, 396, 36 };
-        final double[] calLotsePoint2 =
-        { 54, 11, 396, 831 };
-        final double[] calLotsePoint3 =
-        { 54, 14, 1081, 831 };
+        final double[] calLotsePoint1 = { 56, 11, 396, 36 };
+        final double[] calLotsePoint2 = { 54, 11, 396, 831 };
+        final double[] calLotsePoint3 = { 54, 14, 1081, 831 };
 
         // Kalibrierungs Punkte(S09lkarte) als Vektor (L1, B1, x1, y1)
-        final double[] calS09lPoint1 =
-        { 55.75, 10.67, 711, 424 };
-        final double[] calS09lPoint2 =
-        { 55.58, 10.67, 859, 1818 };
-        final double[] calS09lPoint3 =
-        { 55.58, 10.83, 1648, 1734 };
+        final double[] calS09lPoint1 = { 55.75, 10.67, 711, 424 };
+        final double[] calS09lPoint2 = { 55.58, 10.67, 859, 1818 };
+        final double[] calS09lPoint3 = { 55.58, 10.83, 1648, 1734 };
 
-        lotse = new Map(new ImageIcon("res/Seekarte/lotse1.jpg"), calLotsePoint1, calLotsePoint2, calLotsePoint3);
-        s09l = new Map(new ImageIcon("res/Seekarte/s09l.jpg"), calS09lPoint1, calS09lPoint2, calS09lPoint3);
+        lotse = new Map(new ImageIcon("res/Seekarte/lotse1.jpg"),
+                calLotsePoint1, calLotsePoint2, calLotsePoint3);
+        s09l = new Map(new ImageIcon("res/Seekarte/s09l.jpg"), calS09lPoint1,
+                calS09lPoint2, calS09lPoint3);
         selectedMap = lotse;
 
     }
 
     // Längengrad
-    public double[] getMapCoords()
-    {
-        final double[] v =
-        { mouseX, mouseY, 1 };
+    public double[] getMapCoords() {
+        final double[] v = { mouseX, mouseY, 1 };
         return Matrix.matMult(selectedMap.getTransform(), v);
     }
 
-    public int getMouseX()
-    {
+    public int getMouseX() {
         return mouseX;
     }
 
-    public void setMouseX(final int mouseX)
-    {
+    public void setMouseX(final int mouseX) {
         this.mouseX = mouseX;
         fireModelChanged();
     }
 
-    public int getMouseY()
-    {
+    public int getMouseY() {
         return mouseY;
     }
 
-    public void setMouseY(final int mouseY)
-    {
+    public void setMouseY(final int mouseY) {
         this.mouseY = mouseY;
         fireModelChanged();
     }
 
-    public Map getLotseMap()
-    {
+    public Map getLotseMap() {
         return lotse;
     }
 
-    public Map getS09lMap()
-    {
+    public Map getS09lMap() {
         return s09l;
     }
 
-    public void changeSelectedMap(final Map map)
-    {
+    public void changeSelectedMap(final Map map) {
         final ArrayList<double[]> MapCoords = new ArrayList<>();
 
-        for (int i = 0; i < points.getSize(); i++)
-        {
+        for (int i = 0; i < points.getSize(); i++) {
             final Punkt p = points.getPunktAt(i);
-            final double[] v =
-            { p.getX(), p.getY(), 1.0 };
+            final double[] v = { p.getX(), p.getY(), 1.0 };
             final double[] result = Matrix.matMult(getMapTransform(), v);
             MapCoords.add(result);
         }
-        selectedMap = map;
+
         final Punktliste newList = new Punktliste();
-        for (final double[] ds : MapCoords)
-        {
-            final double[] result = Matrix.matMult(getInverseMapTransform(), ds);
+        selectedMap = map;
+        for (final double[] ds : MapCoords) {
+            final double[] result = Matrix
+                    .matMult(getInverseMapTransform(), ds);
             newList.add(new Punkt((int) result[0], (int) result[1]));
         }
         points = newList;
+        firePointsChanged();
         fireModelChanged();
     }
 
-    public ImageIcon getSelectedMapIcon()
-    {
+    public ImageIcon getSelectedMapIcon() {
         return selectedMap.getImageIcon();
     }
 
-    public void addMapModelListener(final IMapModelListener l)
-    {
+    public void addMapModelListener(final IMapModelListener l) {
         listeners.add(l);
     }
 
-    public void removeMapModelListener(final IMapModelListener l)
-    {
+    public void removeMapModelListener(final IMapModelListener l) {
         listeners.remove(l);
     }
 
-    public void fireModelChanged()
-    {
-        for (final IMapModelListener l : listeners)
-        {
+    public void fireModelChanged() {
+        for (final IMapModelListener l : listeners) {
             l.mapModelChanged(this);
         }
     }
 
-    public void addPoint(final Punkt p)
-    {
+    public void addPoint(final Punkt p) {
         // points.add(point);
         points.add(p);
         firePointsChanged();
     }
 
-    public void removeMarkedPoint()
-    {
+    public void removeMarkedPoint() {
         points.removeMarked();
         firePointsChanged();
     }
 
-    public void firePointsChanged()
-    {
-        for (final IMapModelListener l : listeners)
-        {
+    public void firePointsChanged() {
+        for (final IMapModelListener l : listeners) {
             l.pointsChanged(this);
         }
     }
 
-    public double[][] getMapTransform()
-    {
+    public double[][] getMapTransform() {
         return selectedMap.getTransform();
     }
 
-    public double[][] getInverseMapTransform()
-    {
+    public double[][] getInverseMapTransform() {
         return selectedMap.getInverseTransform();
     }
 
-    public Punktliste getPoints()
-    {
+    public Punktliste getPoints() {
         return points;
     }
 
-    public void moveMarked(final int x, final int y)
-    {
+    public void moveMarked(final int x, final int y) {
         points.moveMarked(x, y);
         firePointsChanged();
     }
 
-    public void mark(final int x, final int y)
-    {
+    public void mark(final int x, final int y) {
         points.mark(x, y);
         firePointsChanged();
 
